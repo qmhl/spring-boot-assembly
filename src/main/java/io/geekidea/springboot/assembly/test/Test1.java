1a2027fa6405711f373fd1bf49111294b1476841
zhangqi1099 <zhangqi1092@jd.com>
2022-10-18 13:13:16 +0800

    add test

---
+++ src/main/java/io/geekidea/springboot/assembly/test/Test1.java
@@ -0,0 +1,10 @@
+package io.geekidea.springboot.assembly.test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("hello ");
+    }
+}
